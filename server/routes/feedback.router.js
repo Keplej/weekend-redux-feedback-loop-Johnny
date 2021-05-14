7f293610d7f90ad722a63f523b9afdc5dbe9b68a
Johnny Keple <Johnny.Keple@gmail.com>
2021-05-14 06:49:15 -0500

    adding in stuff in feedback router

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -19,7 +19,7 @@ router.post('/', (req, res) => {
     
     let queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments") VALUES ($1, $2, $3, $4);`;
 
-    pool.query(queryText, [feedback.feeling])
+    pool.query(queryText, [feedback.feeling, feedback.understanding, feedback.support, feedback.comments])
     // , feedback.understanding, feedback.support, feedback.comments
     .then(resutlt => {
         res.sendStatus(201);
